c3799e820cb038e0c47d12e3bacbeefd7cfbabdf
kemiksizt <87303268+kemiksizt@users.noreply.github.com>
2021-11-25 23:34:26 +0300

    initilazition part
    Controller,launch and tasks added

---
+++ ContactsWebApiProject/ContactsWebApi/Models/Person.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ContactsWebApi
+{
+    public class Person
+    {
+        public string name { get; set;}
+
+        public string surname { get; set; }
+
+        public int age { get; set;}
+
+        public int phone { get; set; }
+
+        public string email { get; set; }
+
+        public string city { get; set; }
+    }
+}
